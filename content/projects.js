383cdd0abc7ededbb82d867f1e34df8780d6c4be
Ranko Liang <rankoliang@gmail.com>
2021-04-19 18:30:39 -0400

    Add odinbook project

--- content/projects.js
+++ content/projects.js
@@ -13,6 +13,18 @@ const projects = [
       visit: 'https://waldo.rankoliang.com/',
     },
   },
+  {
+    title: 'Odinbook',
+    description: faker.lorem.paragraph(),
+    imageProps: {
+      src: '/images/projects/odinbook.png',
+      format: 'xps15',
+    },
+    links: {
+      github: 'https://github.com/rankoliang/odinbook',
+      visit: 'https://odinbook-rl.herokuapp.com',
+    },
+  },
 ];
 
 export default projects;
