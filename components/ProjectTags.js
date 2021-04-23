c6d91d0a58a97cb5de97a4ab6cc5f6ab408bc157
Ranko Liang <rankoliang@gmail.com>
2021-04-22 21:09:53 -0400

    Add redux tag

--- components/ProjectTags.js
+++ components/ProjectTags.js
@@ -4,12 +4,14 @@ import RubyTag from './projectTags/Ruby';
 import RailsTag from './projectTags/Rails';
 import ReactTag from './projectTags/React';
 import RSpecTag from './projectTags/RSpec';
+import ReduxTag from './projectTags/Redux';
 
 const tagComponents = {
   Ruby: RubyTag,
   Rails: RailsTag,
   React: ReactTag,
   RSpec: RSpecTag,
+  Redux: ReduxTag,
 };
 
 const ProjectTags = ({ tags = Object.keys(tagComponents) }) => {
---
+++ components/projectTags/Redux.js
@@ -0,0 +1,13 @@
+import ReduxIcon from '../icons/ReduxIcon';
+import ProjectTagWrapper from '../ProjectTagWrapper';
+
+const ReduxTag = () => {
+  return (
+    <ProjectTagWrapper background="#764abc" outlined>
+      <ReduxIcon />
+      <span>Redux</span>
+    </ProjectTagWrapper>
+  );
+};
+
+export default ReduxTag;
